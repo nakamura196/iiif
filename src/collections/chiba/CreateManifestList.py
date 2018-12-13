49ce29e80ed4de05213d9cfa070a96ed06f97240
satoru196 <na.kamura.1263@gmail.com>
2018-12-13 14:43:45 +0900

    add script for chiba

---
+++ src/collections/chiba/CreateManifestList.py
@@ -0,0 +1,30 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import csv
+from time import sleep
+
+output_path = "manifest_list.csv"
+
+f = open(output_path, 'w')
+
+writer = csv.writer(f, lineterminator='\n')
+writer.writerow(["Manifest"])
+
+url_list = ["https://iiif.ll.chiba-u.jp/main/koisho.shtml", "https://iiif.ll.chiba-u.jp/main/engeisho.shtml", "https://iiif.ll.chiba-u.jp/main/machinoke.shtml", "https://iiif.ll.chiba-u.jp/main/fungi.shtml"]
+
+for url in url_list:
+
+    sleep(1)
+
+    html = urllib.request.urlopen(url)
+
+    # htmlをBeautifulSoupで扱う
+    soup = BeautifulSoup(html, "html.parser")
+
+    li_list= soup.find("ol").find_all("li")
+
+    for li in li_list:
+        manifest_uri = li.find_all("a")[2].get("href")
+        writer.writerow([manifest_uri])
+
+f.close()
